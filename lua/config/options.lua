b081048585dcf4c02770816cce3c76dab961c71d
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-01-21 13:27:40 +0800

    neovide settings and spell lang

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+--- Neovide settings
+if vim.g.neovide then
+  vim.opt.guifont = { "FiraCode NF Retina", ":h12" }
+  vim.g.neovide_transparency = 0.97
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animation_length = 0.15
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_refresh_rate = 144
+end
+
+-- Spelling language
+vim.opt.spelllang = "en,cjk"
