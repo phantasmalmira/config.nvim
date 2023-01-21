1c611d4efcbcdd2d219f054260a815d3eba4d0d2
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-01-21 13:26:51 +0800

    Add telescope-fzf-native

---
+++ lua/plugins/telescope-fzf-native.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    lazy = true,
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    config = function(_, _)
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
