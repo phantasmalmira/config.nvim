97b0a62e8e10f238b6a00a9e8054f4acad376c67
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-01-27 01:46:13 +0800

    Configure: Color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      opts = { colorscheme = "oxocarbon" },
+      opts = { colorscheme = "tokyonight" },
     },
     -- import/override with your plugins
     { import = "plugins" },
@@ -29,7 +29,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "oxocarbon", "habamax" } },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
