153ef2ecd32bfccc0e3bc836e0715a5eed62b3e6
SilverMira <66930495+SilverMira@users.noreply.github.com>
2023-02-18 21:06:44 +0800

    Configure: lualine styles

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,21 +2,18 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      opts.options.section_separators = { left = "", right = "" }
-      opts.options.component_separators = { left = "", right = "" }
-      opts.sections.lualine_a[1] = {
-        "mode",
-        fmt = function(str)
-          return str:sub(1, 1)
-        end,
-        separator = { left = " ", right = "" },
-      }
-      opts.sections.lualine_z = {
+      opts.options.section_separators = { left = "", right = "" }
+      opts.options.component_separators = { left = "", right = "" }
+      opts.sections.lualine_a = {
         {
-          function()
-            return " " .. os.date("%R")
-          end,
-          separator = { left = " ", right = " " },
+          "mode",
+          separator = { left = "", right = "" },
+        },
+      }
+      opts.sections.lualine_b = {
+        {
+          "branch",
+          separator = { left = "", right = "" },
         },
       }
     end,
